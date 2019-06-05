9d7a641d8dc68eadb9c84fe8558ec7dd8716a7e7
tiefenb <markus.tiefenbacher@gmail.com>
2019-06-05 21:18:02 +0200

    setup module

---
+++ tests/setup.php
@@ -0,0 +1,24 @@
+<?php
+
+  if($_ENV['PW_ENV'] !== 'testing') {
+    echo 'This script should only run in the CI server';
+    exit();
+  }
+
+  include_once(__DIR__ . "/../../../../index.php");
+
+  //$esconfig = $modules->getModuleConfigData('ElasticsearchFeeder');
+
+  // Setup Test Module Config
+  $modules->saveConfig('ElasticsearchFeeder', [
+    'es_debug_mode' => 'off',
+    'es_protocol' => 'http',
+    'es_host' => 'es:9200',
+    'es_schema_path' => 'elasticSearchSchema',
+    'es_index_id_prefix' => 'test'
+  ]);
+
+  // Setup Test Schema for processwire default siteprofile
+
+
+  exit();
