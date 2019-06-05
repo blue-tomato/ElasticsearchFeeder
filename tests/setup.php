eca625f12a9aad333bd1e3924f2af92f8a32f4fe
tiefenb <markus.tiefenbacher@gmail.com>
2019-06-05 22:23:18 +0200

    add template setup

--- tests/setup.php
+++ tests/setup.php
@@ -9,16 +9,20 @@
 
   //$esconfig = $modules->getModuleConfigData('ElasticsearchFeeder');
 
+  $testTemplateId = 29;
+  $testTemplateName = 'basic-page';
+
   // Setup Test Module Config
   $modules->saveConfig('ElasticsearchFeeder', [
     'es_debug_mode' => 'off',
     'es_protocol' => 'http',
     'es_host' => 'es:9200',
     'es_schema_path' => 'elasticSearchSchema',
-    'es_index_id_prefix' => 'test'
+    'es_index_id_prefix' => 'testprefix',
+    "es_template_index_$testTemplateId" => "yes",
+    "es_template_indexname_$testTemplateId" => "testindex",
+    "es_template_type_$testTemplateId" => "testtype"
   ]);
-
-  // template ID: 29 basic-page
 
   // Setup Test Schema for processwire default siteprofile
 
