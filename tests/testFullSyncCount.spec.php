93c883785207ded2e4c3f93d5147721ea5d2312a
tiefenb <markus.tiefenbacher@gmail.com>
2019-06-07 21:40:33 +0200

    next try

--- tests/testFullSyncCount.spec.php
+++ tests/testFullSyncCount.spec.php
@@ -1,27 +1,25 @@
 <?php // testFullSyncCount.spec.php
 
-// include PW API
-include_once(__DIR__ . "/../../../../index.php");
-
-// load ElasticsearchFeeder module class
-$ElasticsearchFeeder = $modules->get('ElasticsearchFeeder');
-$template = $templates->get('basic-page');
-
-var_dump($ElasticsearchFeeder);
-var_dump($template);
-
 describe('ES Index after first batchSync', function() {
     beforeEach(function() {
-      $baseUrl = $ElasticsearchFeeder->getElasticSearchUrlBase();
-      $indexName = $ElasticsearchFeeder->getElasticSearchIndexName($template);
-      $query = "q=prefix:{$ElasticsearchFeeder->getIndexPrefix()}";
+
+      // include PW API
+      include_once(__DIR__ . "/../../../../index.php");
+
+      // load ElasticsearchFeeder module class
+      $this->ElasticsearchFeeder = $modules->get('ElasticsearchFeeder');
+      $template = $templates->get('basic-page');
+
+      $baseUrl = $this->ElasticsearchFeeder->getElasticSearchUrlBase();
+      $indexName = $this->ElasticsearchFeeder->getElasticSearchIndexName($template);
+      $query = "q=prefix:{$this->ElasticsearchFeeder->getIndexPrefix()}";
 
       $this->countRequestUrl = "{$baseUrl}/{$indexName}/_count?{$query}";
     });
 
     describe('count', function() {
         it('should return the number of items', function() {
-            $count = $ElasticsearchFeeder->curlJsonGet($this->countRequestUrl, null)["count"];
+            $count = $this->ElasticsearchFeeder->curlJsonGet($this->countRequestUrl, null)["count"];
             assert($count === 3, 'expected 3');
         });
     });
