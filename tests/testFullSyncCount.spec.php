9a743ee7a5ff88d288ce56b9a50585bf78a006f2
tiefenb <markus.tiefenbacher@gmail.com>
2019-06-07 21:01:32 +0200

    test try nr 2

--- tests/testFullSyncCount.spec.php
+++ tests/testFullSyncCount.spec.php
@@ -1,25 +1,24 @@
 <?php // testFullSyncCount.spec.php
 
-use Elasticsearch\ClientBuilder;
-require '../vendor/autoload.php';
-
-$clientBuilder = ClientBuilder::create();
-$clientBuilder->setHosts(['es']);
-$client = $clientBuilder->build();
-var_dump($client->cat()->count(['index' => 'testindex']));
-
-exit();
+// include PW API
+include_once(__DIR__ . "/../../../../index.php");
 
 describe('ES Index after first batchSync', function() {
     beforeEach(function() {
-      $clientBuilder = ClientBuilder::create();
-      $clientBuilder->setHosts(['es']);
-      $this->client = $clientBuilder->build();
+
+      // load ElasticsearchFeeder module class
+      $this->ElasticsearchFeeder = $modules->get('ElasticsearchFeeder');
+
+      $baseUrl = $this->ElasticsearchFeeder->getElasticSearchUrlBase();
+      $indexName = $this->ElasticsearchFeeder->getElasticSearchIndexName($templates->get('basic-page'));
+      $query = "q=prefix:{$ElasticsearchFeeder->getIndexPrefix()}";
+
+      $this->countRequestUrl = "$baseUrl/$indexName/_count?$query";
     });
 
     describe('count', function() {
         it('should return the number of items', function() {
-            $count = $this->client->cat()->count(['index' => 'testindex']);
+            $count = $this->ElasticsearchFeeder->curlJsonGet($this->countRequestUrl, null)["count"];
             assert($count === 3, 'expected 3');
         });
     });
