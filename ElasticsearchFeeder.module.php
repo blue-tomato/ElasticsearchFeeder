8f60985f52ad08cef50c40b75c8506f4e2e24c63
Markus Tiefenbacher <markus.tiefenbacher@gmail.com>
2023-10-03 22:18:25 +0200

    Bump Version

--- ElasticsearchFeeder.module.php
+++ ElasticsearchFeeder.module.php
@@ -16,7 +16,7 @@ class ElasticsearchFeeder extends WireData implements Module, ConfigurableModule
 		return array(
 			'title' => 'ElasticsearchFeeder',
 			'class' => 'ElasticsearchFeeder',
-			'version' => 151,
+			'version' => 201,
 			'summary' => 'Schema-flexible module for getting your page into ElasticSearch',
 			'href' => 'https://github.com/blue-tomato/ElasticsearchFeeder/',
 			'singular' => true,
